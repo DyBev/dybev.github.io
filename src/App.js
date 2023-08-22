077b6ef401d180b06101ddf8bb32f2791398c815
DyBev <54762882+DyBev@users.noreply.github.com>
2023-08-22 19:43:45 +0100

    feat: adding router to the website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomeScreen } from './components/home-screen';
+import { PersonalProjects } from './components/personal-projects';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  },
+  {
+    path: "PersonalProjects",
+    element: <PersonalProjects />,
+  },
+]);
 
 function App() {
   return (
-    <HomeScreen/>
+		<RouterProvider router={router}/>
   );
 }
 
