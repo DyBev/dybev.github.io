e1682265cf188d603d16c9cf199b07ed09c1cb33
DyBev <54762882+DyBev@users.noreply.github.com>
2024-02-28 22:35:10 +0000

    Adding language icons

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -15,6 +15,17 @@ function App() {
 			<span className="big-text">Welcome</span>
 			<p>I am a software developer currently working in web environments</p>
 			<p>Looking for my next problem to solve</p>
+			<section className="languageIcons">
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain-wordmark.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain-wordmark.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-plain-wordmark.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-plain-wordmark.svg" />
+				<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/lua/lua-plain.svg" style={{filter: 'brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(2%) hue-rotate(343deg) brightness(113%) contrast(101%)'}}/>
+			</section>
 			<p className="footer">See more</p>
 			<ChevronDown className="footer" size={32}/>
 		</section>
