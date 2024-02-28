11dda29aebbd40cc80c3a62edc5d992138694a90
DyBev <54762882+DyBev@users.noreply.github.com>
2024-02-28 19:35:08 +0000

    WIP Welcome section

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -10,6 +10,13 @@ function App() {
 				<a>Contact</a>
 			</nav>
 		</header>
+		<section className="welcome">
+			<span className="big-text">Welcome</span>
+			<p>I am a software developer currently working in web environments</p>
+			<p>Looking for my next problem to solve</p>
+			<p className="footer">See more</p>
+			<ChevronDown className="footer" size={32}/>
+		</section>
     </>
   )
 }
