7ed7f4a1b8f35960ad0128546496e0cd7617ffc9
DyBev <54762882+DyBev@users.noreply.github.com>
2024-02-28 11:35:43 +0000

    Removing old files

--- personal-portfolio/src/app/personalProjects/[project]/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import { useParams } from 'next/navigation'
-
-export default function Page({ params }: { params: { slug: string } }) {
-	const data = useParams();
-	console.log(data);
-	return <div>My Post: {params.project}</div>
-}
