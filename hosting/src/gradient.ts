8a77320490d76fed1de4b4829a569548cfcfbf13
DyBev <54762882+DyBev@users.noreply.github.com>
2024-02-28 22:34:47 +0000

    Adding moving gradient to Welcome header

---
+++ hosting/src/gradient.ts
@@ -0,0 +1,21 @@
+let gradientDeg = 90;
+let start_transparent = 0;
+let end_transparent = 100;
+let color = 50;
+let gradient_direction = 0.5;
+
+function randomGradient() {
+	let welcomeText = document.querySelector('section.welcome p.bigText');
+	if (color < 10) {
+		gradient_direction = 0.5;
+	}
+	if (color > 90) {
+		gradient_direction = -0.5;
+	}
+	color += gradient_direction;
+
+	gradientDeg += 0.6;
+	welcomeText.style.backgroundImage = `linear-gradient(45deg, var(--text-primary) ${start_transparent}%, var(--background-brand) ${color}%, var(--text-primary) ${end_transparent}%`;
+}
+
+const gradientInterval = setInterval(() => randomGradient(), 1000/60);
