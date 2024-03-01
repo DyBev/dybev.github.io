9c64180075ed8f3d5f86f3000460c9c153b0d085
DyBev <54762882+DyBev@users.noreply.github.com>
2024-03-01 21:42:56 +0000

    Fixing code styling in gradient.js

--- hosting/src/gradient.ts
+++ hosting/src/gradient.ts
@@ -6,15 +6,17 @@ let gradient_direction = 0.5;
 
 export function randomGradient() {
 	let welcomeText = document.querySelector('section.welcome p.bigText');
-	if (welcomeText) {
-		if (color < 10) {
-			gradient_direction = 0.5;
-		}
-		if (color > 90) {
-			gradient_direction = -0.5;
-		}
-		color += gradient_direction;
-		gradientDeg += Math.random();
-		welcomeText.style.backgroundImage = `linear-gradient(${gradientDeg}deg, var(--text-primary) ${start_transparent}%, var(--background-brand) ${color}%, var(--text-primary) ${end_transparent}%`;
+
+	if (color < 10) {
+		gradient_direction = 0.5;
 	}
+
+	if (color > 90) {
+		gradient_direction = -0.5;
+	}
+
+	color += gradient_direction;
+	gradientDeg += Math.random();
+
+	welcomeText.style.backgroundImage = `linear-gradient(${gradientDeg}deg, var(--text-primary) ${start_transparent}%, var(--background-brand) ${color}%, var(--text-primary) ${end_transparent}%`;
 }
