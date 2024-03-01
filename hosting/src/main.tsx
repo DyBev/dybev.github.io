d2f47f18a31df6aa718c22e88d4d588f0344f86c
DyBev <54762882+DyBev@users.noreply.github.com>
2024-03-01 00:28:09 +0000

    Creating browser router

--- hosting/src/main.tsx
+++ hosting/src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.scss'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+import App from './App.tsx'
+import Work from './Work.tsx'
+
+const router = createBrowserRouter([
+	{
+		element: (
+			<>
+				<Outlet/>
+			</>
+		),
+		children: [
+			{
+				path: "/",
+				element: <App/>,
+			},
+			{
+				path: "/work/:company",
+				element: <Work/>
+			},
+		]
+	},
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+		<RouterProvider router={router}/>
   </React.StrictMode>,
 )
