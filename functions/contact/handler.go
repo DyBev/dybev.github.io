c4d9cd646014490cabf2b541c47d63a930e89b54
dybev <54762882+DyBev@users.noreply.github.com>
2026-09-26 10:45:54 +0100

    chore(whitespace): revert change

--- functions/contact/handler.go
+++ functions/contact/handler.go
@@ -113,7 +113,6 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 			},
 		},
 		ReplyToAddresses: []string{email},
-
 		Source: aws.String(os.Getenv("SOURCE_EMAIL")),
 	}
 
