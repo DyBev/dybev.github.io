91d2ab30ccb0c39907e6c744777c14e4b0845172
dybev <54762882+DyBev@users.noreply.github.com>
2026-09-26 10:43:37 +0100

    fix(typo): in the SOURCE_EMAIL env

--- functions/contact/handler.go
+++ functions/contact/handler.go
@@ -113,7 +113,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 			},
 		},
 		ReplyToAddresses: []string{email},
-		Source: aws.String(os.Getenv("SROUCE_EMAIL")),
+		Source: aws.String(os.Getenv("SOURCE_EMAIL")),
 	}
 
 	resp, err := client.SendEmail(ctx, input)
